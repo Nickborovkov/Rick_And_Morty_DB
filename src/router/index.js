42372abc7a220d7f07a4911fa33d79000360cbf5
Nickborovkov <borovckov@yandex.ru>
2021-10-22 17:30:55 +0300

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomePage from "../components/homePage/HomePage";
+import ErrorPage from "../components/common/errorPage/ErrorPage";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: `/`,
+    name: `home`,
+    component: HomePage,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: `/characters`,
+    name: `characters`,
+    component: () => import(/*webpackChunkName: "Characters"*/`../components/characters/Characters`),
+  },
+  {
+    path: `/characters/character/:id`,
+    props: true,
+    name: `specificCharacter`,
+    component: () => import(/*webpackChunkName: "SpecificCharacter"*/`../components/characters/specificCharacter/SpecificCharacter`),
+  },
+  {
+    path: `/locations`,
+    name: `locations`,
+    component: () => import(/*webpackChunkName: "Locations"*/`../components/locations/Locations`),
+  },
+  {
+    path: `/locations/location/:id`,
+    name: `specificLocation`,
+    component: () => import(/*webpackChunkName: "SpecificLocation"*/`../components/locations/specificLocation/SpecificLocation`),
+  },
+  {
+    path: `/episodes`,
+    name: `episodes`,
+    component: () => import(/*webpackChunkName: "Episodes"*/`../components/episodes/Episodes`),
+  },
+  {
+    path: `/episodes/episode/:id`,
+    name: `specificEpisode`,
+    component: () => import(/*webpackChunkName: "SpecificEpisode"*/`../components/episodes/specificEpisode/SpecificEpisode`),
+  },
+  {
+    path: `*`,
+    name: `errorPage`,
+    component: ErrorPage,
+  },
 ]
 
 const router = new VueRouter({
