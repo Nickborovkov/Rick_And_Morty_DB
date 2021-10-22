028d845f4ae366d3160f73e1d0b69031617c50ce
Nickborovkov <borovckov@yandex.ru>
2021-10-22 17:29:44 +0300

    created store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import charaters from "./charaters";
+import episode from "./episode";
+import locations from "./locations";
+import common from "./common";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    common,
+    charaters,
+    locations,
+    episode,
   }
 })
