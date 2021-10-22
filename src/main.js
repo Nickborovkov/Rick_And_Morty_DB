55b57c99ba659d06de2353c72abcabf0edab4733
Nickborovkov <borovckov@yandex.ru>
2021-10-22 17:32:01 +0300

    init app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from "axios";
 
 Vue.config.productionTip = false
 
+
 new Vue({
   router,
+  axios,
   store,
   render: h => h(App)
 }).$mount('#app')
