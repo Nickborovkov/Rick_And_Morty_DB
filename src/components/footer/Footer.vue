2caa2ba0b12028cedf5dc961dac365c75f6623a7
Nickborovkov <borovckov@yandex.ru>
2021-11-02 15:27:58 +0300

    added footer with links

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>
-        <p>Footer</p>
-    </div>
+    <el-footer>
+        <el-row justify="center">
+            <el-col :xs="24" :sm="12">
+                <el-link
+                        href="https://rickandmortyapi.com/documentation"
+                        type="primary"
+                        icon="el-icon-link"
+                        target="_blank"
+                        class="footerLink"
+                        :underline="false"
+                >
+                    Rick and Morty API
+                </el-link>
+            </el-col>
+            <el-col :xs="24" :sm="12">
+                <el-link
+                        href="https://github.com/Nickborovkov"
+                        type="primary"
+                        icon="el-icon-link"
+                        target="_blank"
+                        class="footerLink"
+                        :underline="false"
+                >
+                    Made by Nick Borovkov
+                </el-link>
+            </el-col>
+        </el-row>
+    </el-footer>
 </template>
 
 <script>
@@ -10,5 +35,10 @@
 </script>
 
 <style scoped>
-
+    .footerLink{
+        text-align: center;
+        display: block;
+        width: 180px;
+        margin: 10px auto;
+    }
 </style>
