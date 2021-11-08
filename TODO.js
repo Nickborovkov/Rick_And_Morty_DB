ae12ba33275695c5d06cafc4020b394b6b82e65a
Nickborovkov <borovckov@yandex.ru>
2021-11-08 17:01:58 +0300

    added TODO.js

---
+++ TODO.js
@@ -0,0 +1,29 @@
+// `todo errors handling when comes empty array in response`
+// `todo errors showing in components`
+
+// `todo locations component`
+// `todo episodes component`
+
+// `todo episodes pagination`
+// `todo locations pagination`
+
+// `todo home component + getBack button`
+// `todo error component  + getBack button`
+
+// `todo preloader component`
+
+// `todo search field unified for all components with queries`
+
+// `todo specificCharacter component`
+// `todo specificLocation component`
+// `todo specificEpisode component`
+
+// `todo back to top button`
+
+// `todo logo in header`
+
+// `todo restyle the whole app`
+
+// `todo check the layout`
+
+// `todo deploy`
