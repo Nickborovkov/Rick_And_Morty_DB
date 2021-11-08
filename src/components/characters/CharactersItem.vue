fbc66e6b04a79f0463aa0d5a952f3386193a0c54
Nickborovkov <borovckov@yandex.ru>
2021-11-08 17:04:57 +0300

    added separate component for character

--- src/components/characters/CharactersItem.vue
+++ src/components/characters/CharactersItem.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
+    <el-col :xs="24" :sm="12" :md="8" :lg="6">
+        <el-card
+                style="margin: 10px"
+                :body-style="{ padding: '0px' }"
+                shadow="hover"
+        >
+            <img
+                    style="width: 100%"
+                    :src="item.image"
+                    class="characterPhoto"
+            >
+            <h2>{{item.name}}</h2>
+        </el-card>
 
-    </div>
+    </el-col>
 </template>
 
 <script>
     export default {
+        props: {
+            item: Object
+        }
     }
 </script>
 
